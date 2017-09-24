1a90a37b4038cc6592bdc6336c16fe4513b23bbd
Kenia Escobar <keniarescobar@gmail.com>
2017-09-24 16:21:15 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var numberToTest = 9;
+
+if ( 30 < numberToTest ) {
+  console.log( 'numberToTest is greater than 30');
+} else if ( numberToTest == 17 ) {
+  console.log( 'numberToTest is 17.')
+} else {
+  console.log( 'numberToTest is not greater than 30 and is not the number 17.' );
+}
+
+console.log( 'at the end of the JS file' );
